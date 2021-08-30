1b0c8011237544f0e34aaa257221bd10e06d198d
David <davidmolinamorales1999@gmail.com>
2021-08-31 01:03:58 +0200

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pa_guacamoleb.mk
+    
+    COMMON_LUNCH_CHOICES := \
+    pa_guacamoleb-user \
+    pa_guacamoleb-userdebug \
+    pa_guacamoleb-eng
